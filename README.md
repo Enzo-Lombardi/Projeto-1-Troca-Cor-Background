390b0b11c52e7def34a043185617ec88547824f9
Enzo Lombardi <enzo.lombardi20@gmail.com>
2023-12-20 21:46:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-1-Troca-Cor-Background
+ Neste projeto eu criei botões para mudar as cores do Background da tela. Para desenvolver isto eu usei as tecnologias: HTML, CSS e JavaScript.
---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById("botao1").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","verde");
+  })
+  
+  document.getElementById("botao2").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","azul");
+  })
+  
+  document.getElementById("botao3").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","vermelho");
+  })
